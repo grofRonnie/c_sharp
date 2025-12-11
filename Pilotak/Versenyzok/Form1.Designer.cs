403bdece350c708efedf6eee2554372378cf2c7f
grofRonnie <miron.mihalics@gmail.com>
2025-12-11 10:59:37 +0100

    Pilotak

---
+++ Pilotak/Versenyzok/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Versenyzok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Feladat3 = new System.Windows.Forms.Button();
+            this.Feladat4 = new System.Windows.Forms.Button();
+            this.Feladat5 = new System.Windows.Forms.Button();
+            this.Feladat6 = new System.Windows.Forms.Button();
+            this.Feladat7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(261, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(420, 425);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Feladat3
+            // 
+            this.Feladat3.Location = new System.Drawing.Point(12, 13);
+            this.Feladat3.Name = "Feladat3";
+            this.Feladat3.Size = new System.Drawing.Size(243, 23);
+            this.Feladat3.TabIndex = 1;
+            this.Feladat3.Text = "3. Feladat";
+            this.Feladat3.UseVisualStyleBackColor = true;
+            this.Feladat3.Click += new System.EventHandler(this.Feladat3_Click);
+            // 
+            // Feladat4
+            // 
+            this.Feladat4.Location = new System.Drawing.Point(12, 43);
+            this.Feladat4.Name = "Feladat4";
+            this.Feladat4.Size = new System.Drawing.Size(243, 23);
+            this.Feladat4.TabIndex = 2;
+            this.Feladat4.Text = "4. Feladat";
+            this.Feladat4.UseVisualStyleBackColor = true;
+            this.Feladat4.Click += new System.EventHandler(this.Feladat4_Click);
+            // 
+            // Feladat5
+            // 
+            this.Feladat5.Location = new System.Drawing.Point(12, 73);
+            this.Feladat5.Name = "Feladat5";
+            this.Feladat5.Size = new System.Drawing.Size(243, 23);
+            this.Feladat5.TabIndex = 3;
+            this.Feladat5.Text = "5. Feladat";
+            this.Feladat5.UseVisualStyleBackColor = true;
+            this.Feladat5.Click += new System.EventHandler(this.Feladat5_Click);
+            // 
+            // Feladat6
+            // 
+            this.Feladat6.Location = new System.Drawing.Point(12, 103);
+            this.Feladat6.Name = "Feladat6";
+            this.Feladat6.Size = new System.Drawing.Size(243, 23);
+            this.Feladat6.TabIndex = 4;
+            this.Feladat6.Text = "6. Feladat";
+            this.Feladat6.UseVisualStyleBackColor = true;
+            this.Feladat6.Click += new System.EventHandler(this.Feladat6_Click);
+            // 
+            // Feladat7
+            // 
+            this.Feladat7.Location = new System.Drawing.Point(12, 133);
+            this.Feladat7.Name = "Feladat7";
+            this.Feladat7.Size = new System.Drawing.Size(243, 23);
+            this.Feladat7.TabIndex = 5;
+            this.Feladat7.Text = "7. Feladat";
+            this.Feladat7.UseVisualStyleBackColor = true;
+            this.Feladat7.Click += new System.EventHandler(this.Feladat7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Feladat7);
+            this.Controls.Add(this.Feladat6);
+            this.Controls.Add(this.Feladat5);
+            this.Controls.Add(this.Feladat4);
+            this.Controls.Add(this.Feladat3);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Feladat3;
+        private System.Windows.Forms.Button Feladat4;
+        private System.Windows.Forms.Button Feladat5;
+        private System.Windows.Forms.Button Feladat6;
+        private System.Windows.Forms.Button Feladat7;
+    }
+}
+
---
+++ Pilotak/Versenyzok/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Versenyzok
+{
+    public partial class Form1 : Form
+    {
+        static List<Driver> drivers = new List<Driver>();
+        public Form1()
+        {
+            InitializeComponent();
+            #region Beolvasás
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader("pilotak.csv");
+                sr.ReadLine();
+
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                    Driver driver1 = new Driver(sor);
+                    drivers.Add(driver1);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+        }
+
+        private void Feladat3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += $"3. feladat: {drivers.Count}";
+        }
+
+        private void Feladat4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += $"\n4. feladat: {drivers.Last().Name}";
+        }
+
+        private void Feladat5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "\n5. feladat:";
+            foreach (var item in drivers)
+            {
+                if (item.Birth < DateTime.Parse("1901.01.01"))
+                {
+                    richTextBox1.Text += $"\n\t{item.Name} ({item.Birth.ToShortDateString()})";
+                }
+            }
+        }
+
+        private void Feladat6_Click(object sender, EventArgs e)
+        {
+            Driver driver = drivers[0];
+            foreach (var item in drivers)
+            {
+                if (item.Number < driver.Number && item.Number != null)
+                {
+                    driver = item;
+                }
+            }
+
+            richTextBox1.Text += $"\n6. feladat: {driver.Origin}";
+        }
+
+        private void Feladat7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "\n7. feladat: ";
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            foreach (var item in drivers)
+            {
+                if (item.Number != null)
+                {
+                    if (numbers.ContainsKey(item.Number.Value))
+                    {
+                        numbers[item.Number.Value]++;
+                    }
+                    else
+                    {
+                        numbers.Add(item.Number.Value, 1);
+                    }
+                }
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item.Value > 1)
+                {
+                    richTextBox1.Text += item.Key+",  ";
+                }
+            }
+        }
+    }
+    class Driver
+    {
+        #region Mezők
+        private string name;
+        private DateTime birth;
+        private string origin;
+        private int? number;
+        #endregion
+
+        public string Name { get { return name; } set { name = value; } }
+        public DateTime Birth { get => birth; set { birth = value; } }
+        public string Origin { get => origin;  set { origin = value; } }
+        public int? Number { get => number; set { number = value; } }
+
+        public Driver(string[] data)
+        {
+             this.Name = data[0];
+             this.Birth = DateTime.Parse(data[1]);
+             this.Origin = data[2];
+             if (data[3] != "")
+             { 
+                this.Number = int.Parse(data[3]);
+             }
+            else this.Number = null;
+        }
+    }
+}
